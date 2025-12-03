21612446ca346969ce5ee3c9165722e87e869937
andrearocchia <andrea.rocchia@agertechnology.it>
2025-12-03 14:16:00 +0100

    Removed CORS dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,42 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { RolesGuard } from './auth/roles.guard';
-import { json, urlencoded } from 'express';
-import { ConfigService } from '@nestjs/config';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  const configService = app.get(ConfigService);
-  const corsEnv = configService.get<string>('CORS_URL');
-
-  const allowedOrigins: string[] = [
-    corsEnv,
-    'http://localhost:5173',
-    'http://localhost',
-    'http://localhost:3000',
-  ].filter((o): o is string => typeof o === 'string' && o.length > 0);
-
-  // CORS multi-origine
-  app.enableCors({
-    origin: allowedOrigins,
-    credentials: true,
-  });
-
-  // Ruoli globali
   const rolesGuard = app.get(RolesGuard);
   app.useGlobalGuards(rolesGuard);
 
-  // === Serve il frontend buildato ===
   const publicPath = join(__dirname, '..', 'public');
 
   app.useStaticAssets(publicPath, {
     prefix: '/',
   });
 
-  // SPA fallback
   app.use((req, res, next) => {
     if (
       !req.url.startsWith('/auth') &&
@@ -51,7 +30,5 @@ async function bootstrap() {
   });
 
   await app.listen(3000);
-  console.log('Backend running on http://localhost:3000');
-  console.log('Allowed origins:', allowedOrigins);
 }
 bootstrap();
